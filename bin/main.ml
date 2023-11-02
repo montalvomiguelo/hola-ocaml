cf888f30d74bc36bcc99d6b1daf8d8ffd5fd2878
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-11-01 20:46:36 -0400

    ✨ Polymorphic function that combines two alpha lists

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,10 @@
-let rec sum l =
-  match l with
-  | [] -> 0
-  | h :: t -> h + sum t
+let rec combine a b =
+  match a with
+  | [] -> b
+  | h :: t -> h :: combine t b
 ;;
 
-print_int (sum [ 1; 2; 3 ]);;
+let result = combine [ 1; 2 ] [ 3; 4 ];;
+
+print_int (List.length result);;
 print_newline ()
